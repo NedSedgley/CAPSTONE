dc40c855157b6955b2677d127ec3d2d07ee6e301
Neil Sedgley <neil.s.sedgley@gmail.com>
2023-12-04 15:30:39 +0000

    added placeholder functions

--- controllers/commutesController.js
+++ controllers/commutesController.js
@@ -4,5 +4,20 @@ const getAllCommutes = (req, res) => {
 const createCommute = (req, res) => {
   res.send('createCommute');
 };
+const getCommute = (req, res) => {
+  res.send('getCommute', req.params.id);
+};
+const updateCommute = (req, res) => {
+  res.send('updateCommute', req.params.id);
+};
+const deleteCommute = (req, res) => {
+  res.send('deleteCommute', req.params.id);
+};
 
-export default { getAllCommutes, createCommute };
+export default {
+  getAllCommutes,
+  createCommute,
+  getCommute,
+  updateCommute,
+  deleteCommute,
+};
